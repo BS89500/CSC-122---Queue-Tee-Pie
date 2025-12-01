d479c42376546c1b38ae3693dbcabc34ecdc5dfc
BS89500 <sb89500@mail.harpercollege.edu>
2025-11-30 23:40:40 -0600

    init commit w/ test

--- tests/hello_test.cpp
+++ tests/hello_test.cpp
@@ -4,7 +4,38 @@
 #include <catch2/generators/catch_generators_range.hpp>
 
 #include "../src/hello.hpp"
+#include "../src/puppy.h"
+#include "../src/kitty.h"
+#include "../src/pygmymarmoset.h"
+#include "../src/queue.h"
 
 TEST_CASE( "it returns Hello World" ) {
-    REQUIRE( hello() == "Hello World!" );
+    SECTION("Complete driver code scenario") {
+        Puppy puppy;
+        Kitty kitty;
+        PygmyMarmoset marmoset;
+
+        QueueTees queue;
+
+        // Initial size should be 0
+        int initial_size = queue.size();
+        REQUIRE(initial_size == 0);
+
+        queue.enqueue(puppy);
+        queue.enqueue(kitty);
+        queue.enqueue(marmoset);
+
+        // Size after enqueuing should be 3
+        int size_after_enqueue = queue.size();
+        REQUIRE(size_after_enqueue == 3);
+
+        // Dequeue all three
+        queue.dequeue();  // puppy
+        queue.dequeue();  // kitty
+        queue.dequeue();  // marmoset
+
+        // Queue should now be empty
+        REQUIRE(queue.size() == 0);
+        REQUIRE(queue.isEmpty() == true);
+    }
 }
